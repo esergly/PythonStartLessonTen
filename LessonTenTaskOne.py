04ee07074f30df21d59795abc4b62d08ba075e65
esergly <29374586+esergly@users.noreply.github.com>
2017-12-24 02:12:00 +0200

    Update LessonTenTaskOne.py

--- LessonTenTaskOne.py
+++ LessonTenTaskOne.py
@@ -1,7 +1,6 @@
 def max_of_list(my_list):
-    my_list.sort()
-    return my_list[len(my_list) - 1]
+    return print(max(my_list))
 
 
 my_list = [5, 7, 8, 1, 2, 9, 5, 5, 3]
-print(str(max_of_list(my_list)))
+max_of_list(my_list)
